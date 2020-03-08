c13d582464043ec536f40579c9aeb0430d00649b
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-03-08 16:01:59 +0200

    add shop.sagas.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,17 @@
 import {createStore,applyMiddleware} from 'redux';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
 /*this is something that is nice for us to use
 when debugging our redux code*/
+import createSagaMiddleware from 'redux-saga';
 import {persistStore} from 'redux-persist';
 /*allows our browser to actually cache our store*/
+import {fetchCollectionsStart} from './shop/shop.sagas';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if(process.env.NODE_ENV === 'development'){
   middlewares.push(logger);
@@ -16,4 +19,5 @@ if(process.env.NODE_ENV === 'development'){
 
 export const store = createStore(rootReducer,applyMiddleware(...middlewares));
 /*applyMiddleware(...middlewares) for know the prev state,action,next state*/
+sagaMiddleware.run(fetchCollectionsStart);
 export const persistor = persistStore(store);
